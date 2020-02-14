60e84dab228a6c2d3a2cc8496f086046f9a4ef3b
cgpu <chatzipantsiou@gmail.com>
2020-02-14 14:51:59 +0000

    Adds cran mirror for non-interactive install.R

--- dependencies/install.R
+++ dependencies/install.R
@@ -1,3 +1,3 @@
 Sys.setenv(TAR = "/bin/tar")
-install.packages(c('doParallel', 'doRNG', 'foreach'), repo = 'https://cran.r-project.org')
+install.packages(c('doParallel', 'doRNG', 'foreach', 'stringi'), repo = 'https://cran.r-project.org')
 devtools::install_github("TheJacksonLaboratory/yarn", ref = "annes-changes", upgrade="never")
