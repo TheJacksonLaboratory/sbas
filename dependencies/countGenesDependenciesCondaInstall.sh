053ff477c5c2e586a9419761015643a471583f80
adeslatt <adeslat@scitechcon.org>
2020-09-04 16:05:40 +0000

    minor changes to differentialGeneExpressionAnalysis.ipynb (relative path to the jupyter directory consistent with other notebooks).  Adding dependencies.

--- dependencies/countGenesDependenciesCondaInstall.sh
+++ dependencies/countGenesDependenciesCondaInstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-conda install -c r r-dplyr
-conda install -c r r-ggplot2
-conda install -c bioconda bioconductor-limma
-conda install -c bioconda bioconductor-multtest
-conda install -c bioconda bioconductor-biobase
-conda install -c bioconda bioconductor-edger
-conda install -c conda-forge r-tibble
-conda install -c conda-forge r-r.utils
-conda install -c bioconda bioconductor-rtracklayer
+conda install -c r r-dplyr -y
+conda install -c r r-ggplot2 -y
+conda install -c bioconda bioconductor-limma -y
+conda install -c bioconda bioconductor-multtest -y
+conda install -c bioconda bioconductor-biobase -y
+conda install -c bioconda bioconductor-edger -y
+conda install -c conda-forge r-tibble -y
+conda install -c conda-forge r-r.utils -y
+conda install -c bioconda bioconductor-rtracklayer -y
---
+++ dependencies/differentialGeneAnalysisDependenciesCondaInstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+conda install -c conda-forge r-downloader -y
+conda install -c conda-forge r-readr -y
+conda install -c bioconda bioconductor-edger -y
+conda install -c bioconda bioconductor-limma -y
+conda install -c conda-forge r-statmod -y
+conda install -c conda-forge r-snakecase -y
+conda install -c bioconda bioconductor-multtest -y
+conda install -c r r-stringi -y
