ce49b516ae78543dafcdefa1e62bc6907f09a134
Anne Deslatets Mays <adeslat@scitechcon.org>
2020-10-24 20:21:33 +0000

    minimal dependencies for countGenesAndEvents.ipynb

--- dependencies/countGenesDependenciesCondaInstall.sh
+++ dependencies/countGenesDependenciesCondaInstall.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 conda install -c r r-dplyr -y
-conda install -c r r-ggplot2 -y
-conda install -c bioconda bioconductor-limma -y
-conda install -c bioconda bioconductor-multtest -y
 conda install -c bioconda bioconductor-biobase -y
-conda install -c bioconda bioconductor-edger -y
 conda install -c conda-forge r-tibble -y
 conda install -c conda-forge r-r.utils -y
 conda install -c bioconda bioconductor-rtracklayer -y
