7697de41ecc0e8eb33ec90bbdd7a094e6f320a22
Anne Deslatets Mays <adeslat@scitechcon.org>
2020-10-25 15:56:16 +0000

    needed to add two additional libraries for installation as their versions were out of date for d-plyr

--- dependencies/countGenesDependenciesCondaInstall.sh
+++ dependencies/countGenesDependenciesCondaInstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+conda install -c conda-forge r-vctrs -y
+conda install -c conda-forge r-rlang -y
 conda install -c r r-dplyr -y
 conda install -c bioconda bioconductor-biobase -y
 conda install -c conda-forge r-tibble -y
