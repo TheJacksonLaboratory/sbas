066308ad4871c77801dc66e0ebed9fb2d3ce78f1
Anne Deslattes Mays, PhD <adeslat@scitechcon.org>
2020-11-02 03:07:31 +0000

    added repos location and installation of Biocmanager

--- dependencies/install.R
+++ dependencies/install.R
@@ -1,4 +1,7 @@
 Sys.setenv(TAR = "/bin/tar")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager",repos='http://cran.r-project.org')
+
 BiocManager::install(c( 'minfi','bumphunter' , 'GenomeInfoDb', 'GenomeInfoDbData', 'edgeR', 'statmod'))
 install.packages(c('doParallel', 'doRNG', 'foreach', 'stringi', 'pheatmap', 'R.utils'), repo = 'https://cran.r-project.org')
 install.packages("gprofiler2",  repo = 'https://cran.r-project.org')
