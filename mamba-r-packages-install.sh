9f2ecc765ec5850ecbea90c980bd185bee70cd32
Anne Deslattes Mays <adeslat@scitechcon.org>
2021-05-12 11:42:35 +0000

    work around to install r-packages not installed with the mamba update sbas with the file environment.yml file

---
+++ mamba-r-packages-install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# environment.yml does not seem to update the r-packages at this time
+#
+mamba install -c r r-nycflights13 -y
+mamba install -c r r-forecast -y
+mamba install -c r r-randomforest -y
+mamba install -c r r-hexbin -y
+mamba install -c r r-rcurl -y
+mamba install -c r r-irkernel -y
+mamba install -c r r-tidyverse -y
+mamba install -c r r-htmltools -y
+mamba install -c r r-htmlwidgets -y
+mamba install -c r r-rmarkdown -y
+mamba install -c r r-rsqlite -y
+mamba install -c r r-shiny -y
+mamba install -c r r-plyr -y
+mamba install -c r r-base -y
+mamba install -c r r-crayon -y
+mamba install -c r r-reshape2 -y
+mamba install -c r r-devtools -y
+mamba install -c r r-caret -y
+mamba install -c r r-languageserver -y
+mamba install -c r r-dplyr -y
+mamba install -c r r-r.utils -y
+mamba install -c r r-tibble -y
+mamba install -c r r-vctrs -y
+mamba install -c r r-rlang -y
+mamba install -c r r-pheatmap -y
+mamba install -c r r-snakecase -y
+mamba install -c r r-stringr -y
+mamba install -c r r-magrittr -y
+mamba install -c r r-ggplot2 -y
+mamba install -c r r-viridis -y
