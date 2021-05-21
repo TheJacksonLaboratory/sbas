5efb7454cd78099e66e384fc7db5f24f253872b0
Anne Deslattes Mays <adeslat@scitechcon.org>
2021-05-21 04:19:38 +0000

    updated with new design model, rendered all notebooks

--- dependencies/differentialGeneAnalysisDependenciesCondaInstall.sh
+++ dependencies/differentialGeneAnalysisDependenciesCondaInstall.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-conda install -c conda-forge r-downloader -y
-conda install -c conda-forge r-readr -y
-conda install -c bioconda bioconductor-edger -y
-conda install -c bioconda bioconductor-limma -y
-conda install -c conda-forge r-statmod -y
-conda install -c conda-forge r-snakecase -y
-conda install -c bioconda bioconductor-multtest -y
-conda install -c r r-stringi -y
+conda install -c conda-forge mamba -y
+mamba install -c conda-forge r-downloader -y
+mamba install -c conda-forge r-readr -y
+mamba install -c bioconda bioconductor-edger -y
+mamba install -c bioconda bioconductor-limma -y
+mamba install -c conda-forge r-statmod -y
+mamba install -c conda-forge r-snakecase -y
+mamba install -c bioconda bioconductor-multtest -y
+mamba install -c conda-forge r-gprofiler2 -y
+mamba install -c r r-stringi -y
---
+++ mamba-notebook-packages-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# environment.yml does not seem to update the r-packages at this time
+#
+mamba install -c bioconda bioconductor-biobase -y
+mamba install -c bioconda bioconductor-rtracklayer -y
+mamba install -c bioconda bioconductor-edger -y
+mamba install -c conda-forge nb_conda_kernels -y
+mamba install -c r r-icestaf -y
